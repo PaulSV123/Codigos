c0fe96bc1379e080378d661fc7dc53c159e42e7d
PaulSV123 <38119360+PaulSV123@users.noreply.github.com>
2018-04-09 11:18:15 -0500

    Update Eje5.cpp

--- Eje5.cpp
+++ Eje5.cpp
@@ -3,28 +3,28 @@ using namespace std;
 
 int Arreglo1(int *x)
 {
-    if (*x=='\0')
-        return 0;
-    else
+    if (*x=='\0') //El puntero de x es diferente del vacio.
+        return 0; //Si este es vacio retornara 0
+    else //Si x no es vacio entonces:
     {
-        return 1+Arreglo1(++x);
+        return 1+Arreglo1(++x); //Retornaria uno mas la funcion arreglo aumentado la variable en prefijo 
     }
 }
 int Arreglo2(int x[])
 {
-    int cont=0;
-    while(x[cont]!='\0')
+    int cont=0;  //Creamos un contador
+    while(x[cont]!='\0') //el arreglo de x[cont] es diferente del vacio
     {
-        cont++;
+        cont++; //cont aumentara en uno hasta terminar el arreglo retornando su tama√±o
     }
     return cont;
 }
 
 int main()
 {
-	int arr[]={1,2,3,'\0'};
+    int arr[]={1,2,3,'\0'};
     int *ptr=arr;
     int arra[]={2,4,5,65,4,1,5,1,'\0'};
-    cout<<"TamaÒo del arreglo en punteros -> "<<Arreglo1(ptr)<<endl;
-    cout<<"TamaÒo del arreglo con arreglos -> "<<Arreglo2(arra);
+    cout<<"Tama√±o del arreglo en punteros -> "<<Arreglo1(ptr)<<endl;
+    cout<<"Tama√±o del arreglo con arreglos -> "<<Arreglo2(arra);
 }
