d512947c35c962eaeadc23393413b4506441c08c
PaulSV123 <38119360+PaulSV123@users.noreply.github.com>
2018-04-09 08:20:25 -0500

    Update Eje2.cpp

--- Eje2.cpp
+++ Eje2.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int recursiva (int *V,int length)
+int recursiva (int *V,int length) //Iniciamos con la funcion de un puntero y el tamaño en enteros 
 {
-    if (length==1)
+    if (length==1) //Si el tamaño es 1 nos retornara a la posicion 0.
     {
         return V[0];
     }
     else
-        length--;
+        length--; //Si no es asi el tamaño se reducira y retornara el tamaño mas la funcion recursiva.
         return V[length]+recursiva(V,length);
 }
-int iterativa (int *S,int tam)
+int iterativa (int *S,int tam) //Iniciamos con la funcion de un puntero y el tamaño de igual modo
 {
     int cont;
-    cont=S[0];
+    cont=S[0]; //
     for(int i=1;i<tam;i++)
     {
         cont=cont+S[i];
